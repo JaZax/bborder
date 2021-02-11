bff6e41fa5dd813548bc38ee0cc6685c850e250c
JaZax <iza.janeczekk@gmail.com>
2021-02-11 20:49:09 +0100

    hierarchy

--- popup/popup.js
+++ popup/popup.js
@@ -1,45 +1,37 @@
 const wrap = document.getElementById('wrap')
-let body
 
+//send request for jsoned dom
 window.onload = () => {
     let qInfo = {
         active: true,
         currentWindow: true
     }
-
     chrome.tabs.query(qInfo, getTab)
-
     function getTab(tabs){
         wrap.innerHTML = ''
-
         chrome.tabs.sendMessage(tabs[0].id, 'get')
     }    
 }
 
 // receive data from content
 chrome.runtime.onMessage.addListener((msg)=>{
-
-    // body = domJSON.toDOM(msg)
-    // document.body.appendChild(body)
-
     for(id in msg.node.childNodes){
-        //console.log(msg.node.childNodes[id])
         tree(msg.node.childNodes[id], wrap)
     }
-
 })
 
+// create a dom tree 
 let tree = (elem, parent) => {
     let newEl = document.createElement('div')
         newEl.id = elem.id
         newEl.innerText = elem.id
+        newEl.classList = 'parent'
 
+        // works only when there arent any divs with the same id
         document.getElementById(parent.id).appendChild(newEl)
 
     if(elem.childNodes.length > 0){
         for(id in elem.childNodes){
-            //console.log(elem.childNodes[id].id)
-            console.log(elem.id)
             tree(elem.childNodes[id], elem)
         }
     }
